d9a5f2445cd4e3aa1bbec23266d830ee1e3e3cb3
magululukas <morcusmagulu@gmail.com>
2021-12-02 14:56:36 -0800

    printing size of datatypes.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,15 +8,20 @@
 #include <stdio.h>
 int main(void)
 {
-        char a;
-        int b;
-        long int c;
-        long long int d;
-        float e;
-        printf("Size of a char: %d byte(s)\n", sizeof a);
-	printf("Size of an int: %d byte(s)\n", sizeof b);
-	printf("Size of a long int: %1d byte(s)\n", sizeof c);
-	printf("Size of a long long int: %1u byte(s)\n", sizeof d);
-	printf("Size of a float: %1u byte(s)\n", sizeof e);
+	char a;
+
+	int b;
+
+	long int c;
+
+	long long int d;
+
+	float e;
+
+	printf("Size of a char: %d byte(s)\n", sizeof(a));
+	printf("Size of an int: %d byte(s)\n", sizeof(b));
+	printf("Size of a long int: %1d byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %1u byte(s)\n", sizeof(d));
+	printf("Size of a float: %1u byte(s)\n", sizeof(e));
 	return (0);
 }
