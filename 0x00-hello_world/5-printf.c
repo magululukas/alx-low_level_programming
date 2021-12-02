c3cf1a09a828e2085ba5d1090c2248936faa0948
magululukas <morcusmagulu@gmail.com>
2021-12-02 13:28:53 -0800

    So fresh to printf() function!

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/**
+ * main - Starting point
+ *
+ * printf - Printing out astring
+ *
+ * Return: zero Always
+ */
+#include <stdio.h>
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
