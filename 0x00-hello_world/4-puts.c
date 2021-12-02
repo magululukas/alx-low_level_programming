ff22172f028f3ab2a77b4355991592225d5a2abc
magululukas <morcusmagulu@gmail.com>
2021-12-02 12:40:31 -0800

    betty style included.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 /*A programe to print a statement usins the puts function*/
 #include <stdio.h>
 #include <string.h>
-int main()
+int main(void)
 {
 	puts("\"\"Programming is like building a multilingual puzzle");
-        return (0);
+	return (0);
 }
