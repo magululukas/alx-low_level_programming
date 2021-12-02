017bba344fc9b6b39dfa0c53db4eca262d82d60c
magululukas <morcusmagulu@gmail.com>
2021-12-02 12:22:08 -0800

    use of puts() function.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 /*A programe to print a statement usins the puts function*/
 #include <stdio.h>
+#include <string.h>
 int main()
 {
-	puts(""Programming is like building a multilingual puzzle");
-	return (0);
+	puts("\"\"Programming is like building a multilingual puzzle");
+        return (0);
 }
