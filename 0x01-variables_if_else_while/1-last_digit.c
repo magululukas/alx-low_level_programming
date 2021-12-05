ff01d750d6dc70ed62ee52a0f5c1796ff3fa2952
magululukas <morcusmagulu@gmail.com>
2021-12-05 03:13:02 -0800

    If clauses.git status

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Stats here
+ *
+ * printf - Printing out strings
+ *
+ * Return: zero always
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	int last_digit;
+	
+	last_digit = n % 10;
+	if (last_digit > 5)
+	{
+	printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
+	}
+	if (last_digit == 0)
+	{
+	printf("Last digit of %d is %d and is 0\n", n, last_digit);
+	}
+	if (last_digit < 6 != 0)
+	{
+	printf("Last digit of %d is %d and is 0\n", n, last_digit);
+	} /* your code goes there */
+	return (0);
+}
