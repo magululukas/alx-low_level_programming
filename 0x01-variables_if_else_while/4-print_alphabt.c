f3f75314b96e3d44a4101354897a46c46ebd55f3
magululukas <morcusmagulu@gmail.com>
2024-06-30 03:16:17 -0700

    using the continue statement

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - function prints alphabet except q and e
+ *
+ * Return: its zero
+ */
+int main(void)
+{
+	char a;`
+	    
+	for (a = 'a'; a < 'z'; a++)
+	{
+		putchar(a);
+		if (a = 'q' || a = 'e')
+			continue;
+	}
+	putchar('\n');
+	return (0);
+}
