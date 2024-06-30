8117efc8b2de551cd29a30bea75750165c0eb01f
magululukas <morcusmagulu@gmail.com>
2024-06-30 14:26:33 -0700

    useed for loop

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers
+ *
+ * Return: rerurns an integer
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
