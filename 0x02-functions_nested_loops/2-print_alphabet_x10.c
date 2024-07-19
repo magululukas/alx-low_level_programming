21a45c08d53b9b7527a17194e09b27725d08cb6a
magululukas <morcusmagulu@gmail.com>
2024-07-19 04:51:03 -0700

    code prints the alphabet 10times but note that the loop considers integers first.

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times.
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int a = 0;
+
+	while (a <= 10)
+	{
+		char b = 'a';
+
+		while (b <= 'z')
+		{
+			_putchar(b);
+			b++;
+		}
+		a++;
+		_putchar('\n');
+	}
+}
+
