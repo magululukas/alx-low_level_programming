c18349ae45c76e741b5ad7a14c8432d29f0fff26
magululukas <morcusmagulu@gmail.com>
2024-07-21 15:56:02 -0700

    addition function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - adds two ints
+ * Return: added value
+ * @a: variable
+ * @b: variable
+ */
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
