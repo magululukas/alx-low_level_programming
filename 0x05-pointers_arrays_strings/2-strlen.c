7aa57a1ae279bbb2824afd172a98890d9fbe3b11
magululukas <morcusmagulu@gmail.com>
2024-08-05 14:51:04 -0700

    pointers concept again

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - prints the length of a given string
+ * @s: its a pointer variable
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+	int result = strlen(s);
+
+	return (result);
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - prints a string to stdout
+ * @str: its a pointer
+ * Return: returns nohjing
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
