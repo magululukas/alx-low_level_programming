30b1380fb096e26c9e3768028d1577e03f5829f7
magululukas <morcusmagulu@gmail.com>
2025-03-02 13:50:46 -0800

    using the write function to std err

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,18 @@
-#!/bin/bash
-echo "and that piece of art is useful" - Dora Korpar, 2015-10-1
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints a specific message to standard error.
+ *
+ * Return: Always returns 1.
+ */
+int main(void)
+{
+	char message[] = "and that piece of art is useful\" - Dora Korpar, "
+			"2015-10-19\n";
+
+	write(2, message, sizeof(message) - 1);
+	return (1);
+}
+
